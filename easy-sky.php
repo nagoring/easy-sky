6d00205cad13f5746cfb09ab9d8324128480604b
nagomi <nagomi.github@gmail.com>
2017-03-12 22:14:09 +0900

    start

---
+++ easy-sky.php
@@ -0,0 +1,29 @@
+﻿<?php
+/**
+ * Plugin Name:     easy-sky
+ * Plugin URI:      プラグインのURLを記述してください
+ * Description:     プラグインの説明です
+ * Author:          作者名です
+ * Author URI:      作者のURLを記述してください
+ * Version:         バージョンを書いてください
+ */
+
+if(is_admin()){
+	add_action('admin_menu', 'easy_sky_add_menu');
+}
+function easy_sky_add_menu(){
+	add_menu_page('EasySky設定画面', 'EasySky', 'administrator', 'easy_sky_setting', 'easy_sky_setting');
+}
+function easy_sky_setting(){
+    if(isset($_POST['submit'])){
+        $email = $_POST['email'];
+        $mouse = $_POST['mouse'];
+        update_option('Nagomi_EasySky_email', $email);
+        update_option('Nagomi_EasySky_mouse', $mouse);
+    }
+	$email = get_option('Nagomi_EasySky_email');
+	$mouse = get_option('Nagomi_EasySky_mouse');
+
+    include __DIR__ . '/views/v_easy_sky_setting.php';
+}
+
---
+++ views/v_easy_sky_setting.php
@@ -0,0 +1,11 @@
+﻿
+
+<form method="post">
+	<p>メールアドレス</p>
+	<input type="text" name="email" value="<?= $email?>" />
+	<p>ご利用マウス</p>
+	<input type="text" name="mouse" value="<?= $mouse?>" />
+	<input type="submit" name="submit" value="送信" />
+</form>
+
+git@github.com:nagoring/wp-tech2.git
